20859b5a9fc92a15ed0cec87f39a5fcfa3c6e84e
Frank Meza <frankmezapdx@gmail.com>
2018-11-25 13:54:23 -0800

    write test shells for test sitka

---
+++ __tests__/sitka_test.ts
@@ -0,0 +1,39 @@
+import { Sitka } from "../src/sitka"
+
+import TestSitka from "./test_data/test_sitka"
+
+describe("Sitka", () => {
+    const sitka: Sitka = new Sitka()
+
+    describe("public setDispatch()", () => {
+
+    })
+
+    describe("public getModules()", () => {
+
+    })
+
+    describe("public createSitkaMeta()", () => {
+
+    })
+
+    describe("public createStore()", () => {
+
+    })
+
+    describe("public register()", () => {
+
+    })
+
+    describe("private getDefaultState()", () => {
+
+    })
+
+    describe("private createRoot()", () => {
+
+    })
+
+    describe("private doDispatch()", () => {
+
+    })
+})
---
+++ __tests__/test_data/test_sitka.ts
@@ -0,0 +1,7 @@
+import { Sitka } from "../../src/sitka"
+
+import { AppModules } from "./test_index"
+
+export default class TestSitka extends Sitka<AppModules> {
+
+}
