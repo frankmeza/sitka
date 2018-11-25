5298fd921e4b769a082e4dfb01a26e7ea53c4902
Frank Meza <frankmezapdx@gmail.com>
2018-11-25 09:46:32 -0800

    add function typedef and new testFunctions

--- __tests__/test_data/test_sitka_module.ts
+++ __tests__/test_data/test_sitka_module.ts
@@ -1,27 +1,37 @@
+import { Action, Middleware } from "redux"
+
+import { SitkaModuleAction } from "../../src/sitka_module_action"
+import { SagaMeta } from "../../src/saga_meta"
+
 import CounterModule from "./test_counter_module"
+import { CounterState } from "./test_index"
 
 export default class TestSitkaModule extends CounterModule {
     constructor() {
         super()
     }
 
-    public testCreateAction() {
+    public testCreateAction(): SitkaModuleAction<CounterState> {
         return this.createAction({ counter: 42 })
     }
 
-    public testCreateActionNull() {
+    public testCreateActionNull(): SitkaModuleAction<CounterState> {
         return this.createAction(null)
     }
 
-    public testCreateActionEmptyObject() {
+    public testCreateActionEmptyObject(): SitkaModuleAction<CounterState> {
         return this.createAction({})
     }
 
-    public testSetState() {
+    public testSetState(): Action<any> {
         return this.setState({ counter: 42 })
     }
 
-    public spyCreateAction() {
-        return this.createAction({})
+    public testProvideMiddleware(): Middleware[] {
+        return this.provideMiddleware()
+    }
+
+    public testProvideSubscriptions(): SagaMeta[] {
+        return this.provideSubscriptions()
     }
 }
