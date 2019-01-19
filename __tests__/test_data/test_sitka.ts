d916a9c3ef4b038b308c017eb740039c83b63c7f
Frank Meza <frankmezapdx@gmail.com>
2019-01-19 11:47:12 -0800

    change order of methods

--- __tests__/test_data/test_sitka.ts
+++ __tests__/test_data/test_sitka.ts
@@ -1,8 +1,11 @@
 import { Sitka } from "../../src/sitka"
-
 import { AppModules } from "./test_index"
 
 export default class TestSitka extends Sitka<AppModules> {
+    public testGetModules(): {} {
+        return this.getModules()
+    }
+
     public testGetDefaultState(): {} {
         // this is a private method
         return this["getDefaultState"]()
